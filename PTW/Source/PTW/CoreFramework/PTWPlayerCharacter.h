07fd497ca0aab5b78e1aee9272c8f380d8b15245
hrslayer <hrslayer@naver.com>
2026-01-21 20:48:34 +0900

    [add] 초기 클래스 추가 및 BP 추가 - PTWBaseCharacter, PTWAttributeSet, PTWAbilitySystemComponent, PTWPlayerCharacter, PTWPlayerState 클래스 추가 - 간단한 회전,이동 인풋액션 및 IMC 추가

---
+++ PTW/Source/PTW/CoreFramework/PTWPlayerCharacter.h
@@ -0,0 +1,46 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PTWBaseCharacter.h"
+#include "InputActionValue.h"
+#include "PTWPlayerCharacter.generated.h"
+
+class UCameraComponent;
+class UInputMappingContext;
+class UInputAction;
+
+UCLASS()
+class PTW_API APTWPlayerCharacter : public APTWBaseCharacter
+{
+	GENERATED_BODY()
+	
+public:
+	APTWPlayerCharacter();
+
+protected:
+	virtual void BeginPlay() override;
+	virtual void PossessedBy(AController* NewController) override;
+	virtual void OnRep_PlayerState() override;
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	virtual void InitAbilityActorInfo() override;
+
+protected:
+	void Move(const FInputActionValue& Value);
+	void Look(const FInputActionValue& Value);
+
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera")
+	TObjectPtr<UCameraComponent> PlayerCamera;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+	TObjectPtr<UInputMappingContext> DefaultMappingContext;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+	TObjectPtr<UInputAction> MoveAction;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+	TObjectPtr<UInputAction> LookAction;
+
+public:
+	FORCEINLINE UCameraComponent* GetPlayerCamera() const { return PlayerCamera; }
+};

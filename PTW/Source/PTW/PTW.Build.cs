ab23d95f4f78e635519e9fe4aa41de480b615019
hrslayer <hrslayer@naver.com>
2026-01-21 17:01:46 +0900

    [add] 초기 언리얼 엔진 프로젝트 파일 추가 및 에셋 추가 - AdbancedSession 플러그인 추가 - 에셋 추가(로우폴리 캐릭터, 라이라 애니메이션, 로우폴리 총 1, 로우폴리 총 2, Money VFX, 총기 화염 VFX, 로우폴리 마을, UI 사운드) - Build.cs 모듈 추가 - DefaultEngine.ini 온라인서브시스템 코드 추가

---
+++ PTW/Source/PTW/PTW.Build.cs
@@ -0,0 +1,48 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class PTW : ModuleRules
+{
+	public PTW(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        // 1. Core & Basic Modules (기본 엔진 모듈)
+        PublicDependencyModuleNames.AddRange(new string[] {
+            "Core",
+            "CoreUObject",
+            "Engine",
+            "InputCore",
+            "EnhancedInput"
+		});
+
+        // 2. GAS (Gameplay Ability System) 필수 모듈
+        PublicDependencyModuleNames.AddRange(new string[] {
+            "GameplayAbilities",
+            "GameplayTags",
+            "GameplayTasks"
+        });
+
+        // 3. UMG & UI 모듈
+        PublicDependencyModuleNames.AddRange(new string[] {
+            "UMG"
+        });
+
+        // 4. Networking & Online Subsystem (스팀/세션 연동용)
+        PublicDependencyModuleNames.AddRange(new string[] {
+            "OnlineSubsystem",
+            "OnlineSubsystemUtils"
+        });
+
+        // Private Dependencies (내부적으로만 사용하는 모듈)
+        PrivateDependencyModuleNames.AddRange(new string[] {
+            "Slate",
+            "SlateCore",
+			"Json",
+            "JsonUtilities",
+			"NetCore"
+
+		});
+    }
+}

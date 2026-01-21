e2aaf7de698abcf5232fee21c2f9d5780292f90c
hrslayer <hrslayer@naver.com>
2026-01-21 17:43:05 +0900

    [add] BaseCharacter 클래스 추가 - 베이스 캐릭터 클래스 추가함

---
+++ PTW/Source/PTW/CoreFramework/PTWBaseCharacter.cpp
@@ -0,0 +1,34 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PTWBaseCharacter.h"
+
+// Sets default values
+APTWBaseCharacter::APTWBaseCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void APTWBaseCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void APTWBaseCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void APTWBaseCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+

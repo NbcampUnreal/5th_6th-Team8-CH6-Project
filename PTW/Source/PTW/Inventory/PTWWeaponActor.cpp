ae53ed9ff512d3ca43710afd2ce923ad10ab1edd
jeongseoggi <93635959+jeongseoggi@users.noreply.github.com>
2026-01-22 11:05:31 +0900

    [Add] : Weapon Actor Muzzle Socket 추가, Mesh 적용

--- PTW/Source/PTW/Inventory/PTWWeaponActor.cpp
+++ PTW/Source/PTW/Inventory/PTWWeaponActor.cpp
@@ -1,26 +1,22 @@
-﻿// Fill out your copyright notice in the Description page of Project Settings.
-
-
+﻿
 #include "PTWWeaponActor.h"
+#include "PTWWeaponData.h"
 
 
-// Sets default values
 APTWWeaponActor::APTWWeaponActor()
 {
-	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
+	bReplicates = true;
+	
+	MuzzleSocket = CreateDefaultSubobject<USceneComponent>(TEXT("MuzzleSocket"));
+	MuzzleSocket->SetupAttachment(RootComponent);
+	
+	WeaponMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("WeaponMesh"));
+	WeaponMesh->SetupAttachment(RootComponent);
 }
 
-// Called when the game starts or when spawned
 void APTWWeaponActor::BeginPlay()
 {
 	Super::BeginPlay();
-	
-}
-
-// Called every frame
-void APTWWeaponActor::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
 }
 

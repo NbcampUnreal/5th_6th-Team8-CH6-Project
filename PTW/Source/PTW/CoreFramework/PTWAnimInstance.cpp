79e9eea73cd3f67b704f0a1556849fb0ea873714
hrslayer <hrslayer@naver.com>
2026-01-22 14:54:23 +0900

    [add] 애님 인스턴스 클래스 및 BP 추가

---
+++ PTW/Source/PTW/CoreFramework/PTWAnimInstance.cpp
@@ -0,0 +1,52 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CoreFramework/PTWAnimInstance.h"
+#include "CoreFramework/PTWBaseCharacter.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "AbilitySystemComponent.h"
+#include "AbilitySystemGlobals.h"
+
+void UPTWAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+	Character = Cast<APTWBaseCharacter>(TryGetPawnOwner());
+	if (Character)
+	{
+		CharacterMovement = Character->GetCharacterMovement();
+
+		ASC = UAbilitySystemGlobals::GetAbilitySystemComponentFromActor(Character);
+	}
+}
+
+void UPTWAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+
+	if (!Character)
+	{
+		Character = Cast<APTWBaseCharacter>(TryGetPawnOwner());
+		if (Character)
+		{
+			CharacterMovement = Character->GetCharacterMovement();
+			ASC = UAbilitySystemGlobals::GetAbilitySystemComponentFromActor(Character);
+		}
+		return;
+	}
+
+	Velocity = CharacterMovement->Velocity;
+	GroundSpeed = Velocity.Size2D();
+
+	bShouldMove = (GroundSpeed > 3.0f) && (CharacterMovement->GetCurrentAcceleration() != FVector::ZeroVector);
+
+	bIsFalling = CharacterMovement->IsFalling();
+
+	bIsCrouching = Character->bIsCrouched;
+
+	if (ASC)
+	{
+		FGameplayTag SprintTag = FGameplayTag::RequestGameplayTag(TEXT("Input.Action.Sprint"));
+		bIsSprinting = ASC->HasMatchingGameplayTag(SprintTag);
+	}
+}
---
+++ PTW/Source/PTW/CoreFramework/PTWAnimInstance.h
@@ -0,0 +1,44 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "GameplayTagContainer.h"
+#include "PTWAnimInstance.generated.h"
+
+class APTWBaseCharacter;
+class UAbilitySystemComponent;
+class UCharacterMovementComponent;
+
+UCLASS()
+class PTW_API UPTWAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+public:
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+
+protected:
+	UPROPERTY(BlueprintReadOnly, Category = "Character")
+	TObjectPtr<APTWBaseCharacter> Character;
+	UPROPERTY(BlueprintReadOnly, Category = "Character")
+	TObjectPtr<UCharacterMovementComponent> CharacterMovement;
+	UPROPERTY(BlueprintReadOnly, Category = "Character")
+	TObjectPtr<UAbilitySystemComponent> ASC;
+
+
+	UPROPERTY(BlueprintReadOnly, Category = "Movement")
+	FVector Velocity;
+	UPROPERTY(BlueprintReadOnly, Category = "Movement")
+	float GroundSpeed;
+	UPROPERTY(BlueprintReadOnly, Category = "Movement")
+	bool bShouldMove;
+	UPROPERTY(BlueprintReadOnly, Category = "Movement")
+	bool bIsFalling;
+	UPROPERTY(BlueprintReadOnly, Category = "Movement")
+	bool bIsCrouching;
+	UPROPERTY(BlueprintReadOnly, Category = "Movement")
+	bool bIsSprinting;
+};

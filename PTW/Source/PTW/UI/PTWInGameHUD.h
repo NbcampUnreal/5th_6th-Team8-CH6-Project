7dae9943c16e4eac4f9112653e99ae52f43a4273
KimSehun-712 <pitpgns314@naver.com>
2026-01-22 11:36:27 +0900

    [Add] HUD, InGameHUD, ShopNameTag 클래스 추가
    [Add] HUD, InGameHUD, ShopNameTag 클래스 추가

---
+++ PTW/Source/PTW/UI/PTWInGameHUD.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "PTWInGameHUD.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PTW_API UPTWInGameHUD : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};

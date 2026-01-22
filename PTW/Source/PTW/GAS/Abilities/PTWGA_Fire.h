c7e4649ea0e06446a9039f5c8c7edda3ad016e99
jeongseoggi <93635959+jeongseoggi@users.noreply.github.com>
2026-01-22 11:38:16 +0900

    [Add] : 총알 발사 어빌리티 클래스 추가

---
+++ PTW/Source/PTW/GAS/Abilities/PTWGA_Fire.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GAS/PTWGameplayAbility.h"
+#include "PTWGA_Fire.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PTW_API UPTWGA_Fire : public UPTWGameplayAbility
+{
+	GENERATED_BODY()
+};

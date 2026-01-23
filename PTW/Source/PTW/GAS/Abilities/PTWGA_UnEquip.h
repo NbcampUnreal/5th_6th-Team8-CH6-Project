5ffb0a75209ce24f25603a4bf045a838d979cb99
jeongseoggi <93635959+jeongseoggi@users.noreply.github.com>
2026-01-23 15:29:20 +0900

    [Add] : 탈착 어빌리티 BP 추가, 탈착 어빌리티 클래스 추가 [Update] : GameplayTag 추가(Weapon.State.UnEquip), Fire 어빌리티 블락 태그 추가(달릴 때),  WeaponActor에서 3인칭, 1인칭용 메쉬 설정 함수 추가

---
+++ PTW/Source/PTW/GAS/Abilities/PTWGA_UnEquip.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GAS/PTWGameplayAbility.h"
+#include "PTWGA_UnEquip.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PTW_API UPTWGA_UnEquip : public UPTWGameplayAbility
+{
+	GENERATED_BODY()
+};

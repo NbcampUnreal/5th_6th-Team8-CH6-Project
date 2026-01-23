f40760197e2196ce89ef35d686b6330a74ec09a7
KimSehun-712 <pitpgns314@naver.com>
2026-01-23 12:27:34 +0900

    [update] playercontroller 와 HUD 연동

--- PTW/Source/PTW/CoreFramework/PTWPlayerController.cpp
+++ PTW/Source/PTW/CoreFramework/PTWPlayerController.cpp
@@ -2,4 +2,50 @@
 
 
 #include "PTWPlayerController.h"
+#include "UI/PTWHUD.h"
+#include "PTWPlayerState.h"
+#include "AbilitySystemComponent.h"
 
+void APTWPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UE_LOG(LogTemp, Error, TEXT("Controller BeginPlay"));
+
+	TryInitializeHUD();
+}
+
+void APTWPlayerController::OnRep_PlayerState()
+{
+	Super::OnRep_PlayerState();
+
+	TryInitializeHUD();
+}
+
+void APTWPlayerController::TryInitializeHUD()
+{
+	UE_LOG(LogTemp, Error, TEXT("Controller TryInitializeHUD"));
+	// HUD 확인
+	TObjectPtr<APTWHUD> PTWHUD = Cast<APTWHUD>(GetHUD());
+	if (!PTWHUD)
+	{
+		return;
+	}
+
+	// PlayerState 확인
+	TObjectPtr<APTWPlayerState> PTWPS = GetPlayerState<APTWPlayerState>();
+	if (!PTWPS)
+	{
+		return;
+	}
+
+	// ASC 확인
+	TObjectPtr<UAbilitySystemComponent> ASC = PTWPS->GetAbilitySystemComponent();
+	if (!ASC)
+	{
+		return;
+	}
+
+	// HUD 초기화
+	PTWHUD->InitializeHUD(ASC);
+}

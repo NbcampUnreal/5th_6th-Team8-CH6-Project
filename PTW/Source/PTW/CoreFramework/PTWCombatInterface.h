74c31ea7162c111323e5c4222d682903a079b1ae
jeongseoggi <93635959+jeongseoggi@users.noreply.github.com>
2026-01-30 18:10:26 +0900

    [Add] : CombatInterface 추가(피격 데미지 배율 반환, 태그 적용) [Fix] : GA_Fire에서 CombatInterface를 캐스팅하여 사용 하는 방식으로 수정(리팩토링 작업이 끝나면, BaseCharacter에 상속하여 순수 가상 함수 구현 예정) [Refactor] : GC_WeaponFire 리팩토링

---
+++ PTW/Source/PTW/CoreFramework/PTWCombatInterface.h
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "GameplayTagContainer.h"
+#include "PTWCombatInterface.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE(MinimalAPI)
+class UPTWCombatInterface : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class PTW_API IPTWCombatInterface
+{
+	GENERATED_BODY()
+public:
+	virtual float GetDamageMultiplier(const FName& BoneName) = 0;
+	
+	virtual void HandleHitReaction(const FGameplayTag& HitTag) = 0;
+	
+};

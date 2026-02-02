60903ded11ba661d42cd7e8b1475c11f2faf7a6f
JJ00ng <whdgur0316@Naver.com>
2026-02-02 20:39:09 +0900

    [Add, Fix] 폭탄 액터 수정, GE,GA 폭발을 위한 타이머 작성

--- PTW/Source/PTW/MiniGame/Item/PTWBombActor.cpp
+++ PTW/Source/PTW/MiniGame/Item/PTWBombActor.cpp
@@ -1,18 +1,25 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "PTWBombActor.h"
 
 #include "AbilitySystemComponent.h"
+#include "AbilitySystemBlueprintLibrary.h"
+#include "Abilities/GameplayAbility.h"
+#include "GameplayAbilitySpec.h"
+#include "GameplayEffect.h"
+
 #include "Components/StaticMeshComponent.h"
 #include "Components/SphereComponent.h"
+
+#include "GAS/PTWBombAttributeSet.h"
+#include "GameplayTagContainer.h"
 
 APTWBombActor::APTWBombActor()
 {
 	PrimaryActorTick.bCanEverTick = false;
 	bReplicates = true;
 
-	// Collision 
+	// Collision
 	Collision = CreateDefaultSubobject<USphereComponent>(TEXT("Collision"));
 	SetRootComponent(Collision);
 	Collision->InitSphereRadius(50.f);
@@ -26,19 +33,12 @@ APTWBombActor::APTWBombActor()
 	BombMesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 
 	// GAS
-	AbilitySystemComponent = CreateDefaultSubobject<UAbilitySystemComponent>(TEXT("AbilitySystemComponent"));
+	AbilitySystemComponent =
+		CreateDefaultSubobject<UAbilitySystemComponent>(TEXT("AbilitySystemComponent"));
 	AbilitySystemComponent->SetIsReplicated(true);
 	AbilitySystemComponent->SetReplicationMode(EGameplayEffectReplicationMode::Mixed);
-}
 
-void APTWBombActor::BeginPlay()
-{
-	Super::BeginPlay();
-	
-	if (AbilitySystemComponent && BombStateTag.IsValid())
-	{
-		AbilitySystemComponent->AddLooseGameplayTag(BombStateTag);
-	}
+	BombAttributeSet = CreateDefaultSubobject<UPTWBombAttributeSet>(TEXT("BombAttributeSet"));
 }
 
 UAbilitySystemComponent* APTWBombActor::GetAbilitySystemComponent() const
@@ -46,4 +46,88 @@ UAbilitySystemComponent* APTWBombActor::GetAbilitySystemComponent() const
 	return AbilitySystemComponent;
 }
 
+void APTWBombActor::BeginPlay()
+{
+	Super::BeginPlay();
 
+	if (!AbilitySystemComponent) return;
+
+	AbilitySystemComponent->InitAbilityActorInfo(this, this);
+
+	// RemainingTime 변화 감지 
+	const UPTWBombAttributeSet* BombAS = AbilitySystemComponent->GetSet<UPTWBombAttributeSet>();
+	if (BombAS)
+	{
+		RemainingTimeChangedHandle =
+			AbilitySystemComponent
+			->GetGameplayAttributeValueChangeDelegate(BombAS->GetRemainingTimeAttribute())
+			.AddUObject(this, &APTWBombActor::HandleRemainingTimeChanged);
+	}
+
+	if (HasAuthority())
+	{
+		if (ExplodeAbilityClass)
+		{
+			AbilitySystemComponent->GiveAbility(FGameplayAbilitySpec(ExplodeAbilityClass, 1, 0));
+		}
+
+		// RemainingTime = 30 세팅
+		ApplyEffectToSelf(SetTimeEffectClass);
+
+		// 1초마다 RemainingTime -1
+		ApplyEffectToSelf(CountdownEffectClass);
+	}
+}
+
+void APTWBombActor::ApplyEffectToSelf(TSubclassOf<UGameplayEffect> EffectClass)
+{
+	if (!AbilitySystemComponent || !EffectClass) return;
+
+	FGameplayEffectContextHandle Ctx = AbilitySystemComponent->MakeEffectContext();
+	Ctx.AddSourceObject(this);
+
+	FGameplayEffectSpecHandle Spec = AbilitySystemComponent->MakeOutgoingSpec(EffectClass, 1.f, Ctx);
+	if (Spec.IsValid())
+	{
+		AbilitySystemComponent->ApplyGameplayEffectSpecToSelf(*Spec.Data.Get());
+	}
+}
+
+void APTWBombActor::HandleRemainingTimeChanged(const FOnAttributeChangeData& Data)
+{
+	UE_LOG(LogTemp, Warning, TEXT("[Bomb] RemainingTime: %.0f"), Data.NewValue);
+	
+}
+
+void APTWBombActor::RequestExplode(AActor* InstigatorActor)
+{
+	if (!HasAuthority())
+	{
+		ServerRequestExplode(InstigatorActor);
+		return;
+	}
+	
+	if (bExplodeRequested) return;
+	bExplodeRequested = true;
+
+	SendExplodeEvent(InstigatorActor);
+}
+
+void APTWBombActor::ServerRequestExplode_Implementation(AActor* InstigatorActor)
+{
+	RequestExplode(InstigatorActor);
+}
+
+void APTWBombActor::SendExplodeEvent(AActor* InstigatorActor)
+{
+	if (!AbilitySystemComponent) return;
+
+	const FGameplayTag ExplodeTag = FGameplayTag::RequestGameplayTag(TEXT("Event.Bomb.Explode"));
+
+	FGameplayEventData Payload;
+	Payload.EventTag = ExplodeTag;
+	Payload.Instigator = InstigatorActor ? InstigatorActor : this;
+	Payload.Target = this;
+	
+	UAbilitySystemBlueprintLibrary::SendGameplayEventToActor(this, ExplodeTag, Payload);
+}
--- PTW/Source/PTW/MiniGame/Item/PTWBombActor.h
+++ PTW/Source/PTW/MiniGame/Item/PTWBombActor.h
@@ -5,12 +5,16 @@
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
 #include "AbilitySystemInterface.h"
+#include "AbilitySystemComponent.h"
 #include "GameplayTagContainer.h"
 #include "PTWBombActor.generated.h"
 
 class UAbilitySystemComponent;
 class UStaticMeshComponent;
 class USphereComponent;
+class UGameplayEffect;
+class UPTWBombAttributeSet;
+class UGameplayAbility;
 
 UCLASS()
 class PTW_API APTWBombActor : public AActor, public IAbilitySystemInterface
@@ -19,7 +23,7 @@ class PTW_API APTWBombActor : public AActor, public IAbilitySystemInterface
 
 public:
 	APTWBombActor();
-	
+
 	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
 
 protected:
@@ -28,6 +32,9 @@ protected:
 	/** GAS */
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="GAS")
 	TObjectPtr<UAbilitySystemComponent> AbilitySystemComponent;
+
+	UPROPERTY()
+	TObjectPtr<UPTWBombAttributeSet> BombAttributeSet;
 
 	/** Components */
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Components")
@@ -39,4 +46,34 @@ protected:
 	/** 폭탄 상태 태그  */
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="GAS")
 	FGameplayTag BombStateTag;
+
+	/** Timer Effects */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Bomb|GAS")
+	TSubclassOf<UGameplayEffect> SetTimeEffectClass;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Bomb|GAS")
+	TSubclassOf<UGameplayEffect> CountdownEffectClass;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Bomb|GAS")
+	TSubclassOf<UGameplayAbility> ExplodeAbilityClass;
+
+public:
+	UFUNCTION(BlueprintCallable, Category="Bomb")
+	void RequestExplode(AActor* InstigatorActor);
+	
+	UFUNCTION(Server, Reliable)
+	void ServerRequestExplode(AActor* InstigatorActor);
+
+private:
+	void HandleRemainingTimeChanged(const FOnAttributeChangeData& Data);
+
+	FDelegateHandle RemainingTimeChangedHandle;
+
+	void ApplyEffectToSelf(TSubclassOf<UGameplayEffect> EffectClass);
+
+	/** ✅ 내부: GameplayEvent 발사 (Event.Bomb.Explode) */
+	void SendExplodeEvent(AActor* InstigatorActor);
+
+	/** (선택) 중복 폭발 방지용 */
+	bool bExplodeRequested = false;
 };

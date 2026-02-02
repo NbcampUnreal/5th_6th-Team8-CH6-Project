47a4cd532916a7476f568f4250fc4944adcb612a
JJ00ng <whdgur0316@Naver.com>
2026-02-02 14:17:58 +0900

    [add] 폭탄 액터, 어트리븉셋 추가

---
+++ PTW/Source/PTW/GAS/PTWBombAttributeSet.h
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AttributeSet.h"
+#include "AbilitySystemComponent.h"
+#include "PTWBombAttributeSet.generated.h"
+
+#define ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
+GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
+
+UCLASS()
+class PTW_API UPTWBombAttributeSet : public UAttributeSet
+{
+	GENERATED_BODY()
+
+public:
+	UPTWBombAttributeSet();
+
+	/** 남은 시간(초) */
+	UPROPERTY(BlueprintReadOnly, Category="Bomb", ReplicatedUsing=OnRep_RemainingTime)
+	FGameplayAttributeData RemainingTime;
+	ATTRIBUTE_ACCESSORS(UPTWBombAttributeSet, RemainingTime)
+
+protected:
+	UFUNCTION()
+	void OnRep_RemainingTime(const FGameplayAttributeData& OldValue);
+
+public:
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+	virtual void PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue) override;
+};
+

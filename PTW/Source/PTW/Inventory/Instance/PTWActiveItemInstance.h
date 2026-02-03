326ff57536b16485ab7d0b98f7d0d896758a0e9d
jeongseoggi <93635959+jeongseoggi@users.noreply.github.com>
2026-02-03 12:18:37 +0900

    [Add] : Active 아이템 인스턴스 클래스 추가 [Update] : 캐릭터 BP Ability에 GA_BombPistol 추가

---
+++ PTW/Source/PTW/Inventory/Instance/PTWActiveItemInstance.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PTWItemInstance.h"
+#include "PTWActiveItemInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PTW_API UPTWActiveItemInstance : public UPTWItemInstance
+{
+	GENERATED_BODY()
+};

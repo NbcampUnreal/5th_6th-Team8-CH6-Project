63303f8028f97ece8c500dc98cc72f2de45f71aa
Jokimiddle <joki6180@gmail.com>
2026-02-10 20:40:24 +0900

    [update] 패키징중 불필요한 헤더 제거

--- PTW/Source/PTW/Weapon/PTWWeaponCasting.cpp
+++ PTW/Source/PTW/Weapon/PTWWeaponCasting.cpp
@@ -4,7 +4,6 @@
 #include "AbilitySystemBlueprintLibrary.h"
 #include "AbilitySystemComponent.h"
 #include "NaniteSceneProxy.h"
-#include "AssetTypeActions/AssetDefinition_SoundBase.h"
 #include "Kismet/GameplayStatics.h"
 
 

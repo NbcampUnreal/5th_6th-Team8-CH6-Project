8898517a8bf9e3fe7e795390fc8d3d5f922030a2
jeongseoggi <93635959+jeongseoggi@users.noreply.github.com>
2026-02-10 10:43:42 +0900

    [Add] : Melee GA 클래스 추가

---
+++ PTW/Source/PTW/GAS/Abilities/PTWGA_Melee.cpp
@@ -0,0 +1,10 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PTWGA_Melee.h"
+
+void UPTWGA_Melee::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo,
+	const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
+{
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+}
---
+++ PTW/Source/PTW/GAS/Abilities/PTWGA_Melee.h
@@ -0,0 +1,23 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GAS/PTWGameplayAbility.h"
+#include "PTWGA_Melee.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PTW_API UPTWGA_Melee : public UPTWGameplayAbility
+{
+	GENERATED_BODY()
+	
+protected:
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, 
+		const FGameplayAbilityActorInfo* ActorInfo, 
+		const FGameplayAbilityActivationInfo ActivationInfo, 
+		const FGameplayEventData* TriggerEventData) override;
+	
+};

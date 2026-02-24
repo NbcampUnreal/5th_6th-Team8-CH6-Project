d77ade9d6386e7741a5a4121b66f1e3c6dde7b44
jeongseoggi <93635959+jeongseoggi@users.noreply.github.com>
2026-02-24 17:02:25 +0900

    [Update] : Melee 어빌리티 몽타주 재생 코드 추가

--- PTW/Source/PTW/GAS/Abilities/PTWGA_Melee.cpp
+++ PTW/Source/PTW/GAS/Abilities/PTWGA_Melee.cpp
@@ -3,8 +3,14 @@
 
 #include "PTWGA_Melee.h"
 
+#include "Abilities/Tasks/AbilityTask_PlayMontageAndWait.h"
+
 void UPTWGA_Melee::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo,
-	const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
+                                   const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
 {
 	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+	
+	UAbilityTask_PlayMontageAndWait* MontageTask = UAbilityTask_PlayMontageAndWait::CreatePlayMontageAndWaitProxy(
+		this, NAME_None, MeleeAttackMontage);
+	MontageTask->ReadyForActivation();
 }

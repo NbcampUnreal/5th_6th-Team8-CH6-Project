f6bbf81a5458e8041a5d45592ebfd50d5254052a
Jokimiddle <joki6180@gmail.com>
2026-02-24 23:00:11 +0900

    [add] PTWGameSession 생성
    - 데디케이티드 서버로 실행했을 때, 세션이 자동적 생성되도록 구현

---
+++ PTW/Source/PTW/CoreFramework/Game/GameSession/PTWGameSession.cpp
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PTWGameSession.h"
+#include "System/PTWSessionSubsystem.h"
+#include "System/Session/PTWSessionConfig.h"
+
+void APTWGameSession::RegisterServer()
+{
+	Super::RegisterServer();
+	
+	if (IsRunningDedicatedServer())
+	{
+		UPTWSessionSubsystem* SessionSubsystem = GetGameInstance()->GetSubsystem<UPTWSessionSubsystem>();
+		if (SessionSubsystem)
+		{
+			FPTWSessionConfig SessionConfig;
+			SessionConfig.ServerName = TEXT("MyDedicatedServer");
+			SessionConfig.MaxPlayers = 16;
+			
+			const TCHAR* CommandLine = FCommandLine::Get();
+			
+			FString ServerName_cmd = FString::Printf(TEXT("-%s="), *PTWSessionKey::ServerName.ToString());
+			FString MaxPlayers_cmd = FString::Printf(TEXT("-%s="), *PTWSessionKey::MaxPlayers.ToString());
+			
+			FParse::Value(CommandLine, *ServerName_cmd, SessionConfig.ServerName);
+			FParse::Value(CommandLine, *MaxPlayers_cmd, SessionConfig.MaxPlayers);
+			
+			SessionSubsystem->CreateGameSession(SessionConfig);
+		}
+	}
+}

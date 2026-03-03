f186602b872a09c393068e0179035b4f2c867894
jeongseoggi <93635959+jeongseoggi@users.noreply.github.com>
2026-03-03 19:49:37 +0900

    [Add] : MiniGameMode 클래스 추가 및 기본 기능 추가

---
+++ PTW/Source/PTW/MiniGame/GameMode/PTWDeliveryGameMode.cpp
@@ -0,0 +1,48 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PTWDeliveryGameMode.h"
+
+
+#include "AbilitySystemComponent.h"
+#include "CoreFramework/PTWPlayerCharacter.h"
+#include "System/PTWItemSpawnManager.h"
+#include "CoreFramework/PTWPlayerState.h"
+
+APTWDeliveryGameMode::APTWDeliveryGameMode()
+{
+}
+
+void APTWDeliveryGameMode::StartRound()
+{
+	SetMiniGameRule();
+	Super::StartRound();
+}
+
+void APTWDeliveryGameMode::GiveDeliveryItems(APTWPlayerCharacter* TargetCharacter)
+{
+	ApplyMiniGameEffect(TargetCharacter);
+	GivingDefaultWeapon(TargetCharacter);
+}
+
+void APTWDeliveryGameMode::ApplyMiniGameEffect(APTWPlayerCharacter* TargetCharacter)
+{
+	IPTWCombatInterface* CombatInterface = Cast<IPTWCombatInterface>(TargetCharacter);
+	if (!CombatInterface) return;
+	CombatInterface->ApplyGameplayEffectToSelf(DeliveryStartEffect, 1.0f, FGameplayEffectContextHandle());
+}
+
+void APTWDeliveryGameMode::GivingDefaultWeapon(APTWPlayerCharacter* TargetCharacter)
+{
+	UPTWItemSpawnManager* SpawnManager = GetWorld()->GetSubsystem<UPTWItemSpawnManager>();
+	if (!SpawnManager) return;
+	
+	SpawnManager->SpawnSingleItem(TargetCharacter->GetPlayerState<APTWPlayerState>(), DefaultWeaponDef);
+}
+
+void APTWDeliveryGameMode::SetMiniGameRule()
+{
+	MiniGameRule.TimeRule.Timer = 180;
+	MiniGameRule.KillRule.KillScore = 0;
+	MiniGameRule.SpawnRule.RespawnDelay = 3.0f;
+}
---
+++ PTW/Source/PTW/MiniGame/GameMode/PTWDeliveryGameMode.h
@@ -0,0 +1,41 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MiniGame/PTWMiniGameMode.h"
+#include "PTWDeliveryGameMode.generated.h"
+
+class APTWPlayerCharacter;
+/**
+ * 
+ */
+UCLASS()
+class PTW_API APTWDeliveryGameMode : public APTWMiniGameMode
+{
+	GENERATED_BODY()
+	
+public:
+	APTWDeliveryGameMode();
+	virtual void StartRound() override;
+	
+protected:
+	void GiveDeliveryItems(APTWPlayerCharacter* TargetCharacter);
+	
+private:
+	/* 미니 게임 시작 이펙트 적용 */
+	void ApplyMiniGameEffect(APTWPlayerCharacter* TargetCharacter);
+	
+	/* 미니 게임 시작 무기 지급*/
+	void GivingDefaultWeapon(APTWPlayerCharacter* TargetCharacter);
+	
+	/* 미니 게임 룰 적용*/
+	void SetMiniGameRule();
+
+protected:
+	UPROPERTY()
+	TSubclassOf<UGameplayEffect> DeliveryStartEffect;
+	
+	UPROPERTY()
+	TObjectPtr<UPTWItemDefinition> DefaultWeaponDef;
+};

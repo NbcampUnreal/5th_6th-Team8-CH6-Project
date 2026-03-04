7116bf57d97d88a60dc74c52127efdea2fe9b612
Jokimiddle <joki6180@gmail.com>
2026-03-04 19:06:39 +0900

    [add] HTTPRequest 클래스 및 관련파일 생성

---
+++ PTW/Source/PTW/System/Server/PTWHTTPRequestManager.cpp
@@ -0,0 +1,53 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PTWHTTPRequestManager.h"
+
+#include "GameplayServerTags.h"
+#include "PTWAPIData.h"
+#include "HttpModule.h"
+#include "JsonObjectConverter.h"
+#include "PTWHTTPRequestTypes.h"
+#include "Interfaces/IHttpResponse.h"
+
+void UPTWHTTPRequestManager::RequestListFleets()
+{
+	check(APIData);
+	
+	TSharedRef<IHttpRequest> Request = FHttpModule::Get().CreateRequest();
+	
+	Request->OnProcessRequestComplete().BindUObject(this, &ThisClass::ListFleets_Response);
+	
+	const FString APIUrl = APIData->GEtAPIEndPoint(GameplayServerTags::GameSessionsAPI::ListFleets);
+	
+	Request->SetURL(APIUrl);
+	Request->SetVerb(TEXT("GET"));
+	Request->SetHeader(TEXT("Content-Type"), TEXT("application/json"));
+	Request->ProcessRequest();
+	
+	UE_LOG(LogTemp, Warning, TEXT("ListFleets_Response Made"));
+}
+
+void UPTWHTTPRequestManager::ListFleets_Response(FHttpRequestPtr Request, FHttpResponsePtr Response,
+	bool bWasSuccessful)
+{
+	UE_LOG(LogTemp, Warning, TEXT("ListFleets_Response Received"));
+	
+	TSharedPtr<FJsonObject> JsonObject;
+	TSharedRef<TJsonReader<>> JsonReader = TJsonReaderFactory<>::Create(Response->GetContentAsString());
+	if (FJsonSerializer::Deserialize(JsonReader, JsonObject))
+	{
+		if(JsonObject->HasField(TEXT("$metadata")))
+		{
+			TSharedPtr<FJsonObject> MetaDataJsonObject = JsonObject->GetObjectField(TEXT("$metadata"));
+			FPTWMetaData PTWMetaData;
+			FJsonObjectConverter::JsonObjectToUStruct(MetaDataJsonObject.ToSharedRef(), &PTWMetaData);
+			
+			PTWMetaData.Dump();
+		}
+		
+		FPTWListFleetsResponse ListFleetsResponse;
+		FJsonObjectConverter::JsonObjectToUStruct(JsonObject.ToSharedRef(), &ListFleetsResponse);
+		ListFleetsResponse.Dump();
+	}
+}
---
+++ PTW/Source/PTW/System/Server/PTWHTTPRequestTypes.h
@@ -0,0 +1,38 @@
+﻿#pragma once
+
+#include "PTWHTTPRequestTypes.generated.h"
+
+USTRUCT()
+struct FPTWMetaData
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY()
+	int32 httpStatusCode{};
+	
+	UPROPERTY()
+	FString requestId;
+	
+	UPROPERTY()
+	int32 attempts{};
+	
+	UPROPERTY()
+	double totalRetryDelay{};
+	
+	void Dump() const;
+};
+
+USTRUCT()
+struct FPTWListFleetsResponse
+{
+	GENERATED_BODY()
+	
+	UPROPERTY()
+	TArray<FString> FleetIds{};
+	
+	UPROPERTY()
+	FString NextToken;
+	
+	void Dump() const;
+};

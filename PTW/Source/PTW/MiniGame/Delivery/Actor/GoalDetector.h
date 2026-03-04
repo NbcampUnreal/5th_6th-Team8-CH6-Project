acd6d06b5d7c5c8dd26f6462a1eecf96555b4296
jeongseoggi <93635959+jeongseoggi@users.noreply.github.com>
2026-03-04 16:11:10 +0900

    [Fix] : 빌드 오류 수정

--- PTW/Source/PTW/MiniGame/Delivery/Actor/GoalDetector.h
+++ PTW/Source/PTW/MiniGame/Delivery/Actor/GoalDetector.h
@@ -16,6 +16,5 @@ public:
 	AGoalDetector();
 	
 protected:
-	UFUNCTION()
 	virtual void OnDetectPlayer(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult) override;
 };

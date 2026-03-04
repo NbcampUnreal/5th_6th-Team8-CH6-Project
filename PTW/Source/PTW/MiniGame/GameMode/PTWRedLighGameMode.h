77ef7821e53a64ba0bf8b3bb6580433179fe1ec9
hrslayer <hrslayer@naver.com>
2026-03-04 20:32:39 +0900

    [Add] 무궁화꽃이피었습니다 미니게임모드 추가

---
+++ PTW/Source/PTW/MiniGame/GameMode/PTWRedLighGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MiniGame/PTWMiniGameMode.h"
+#include "PTWRedLighGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PTW_API APTWRedLighGameMode : public APTWMiniGameMode
+{
+	GENERATED_BODY()
+	
+};

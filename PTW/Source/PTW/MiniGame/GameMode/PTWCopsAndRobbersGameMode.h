130da36cd1aca242e456610c57b5079ba8421185
Jokimiddle <joki6180@gmail.com>
2026-03-05 19:09:21 +0900

    [update] 경찰과도둑 게임모드 설정 추가

--- PTW/Source/PTW/MiniGame/GameMode/PTWCopsAndRobbersGameMode.h
+++ PTW/Source/PTW/MiniGame/GameMode/PTWCopsAndRobbersGameMode.h
@@ -6,15 +6,23 @@
 #include "MiniGame/PTWMiniGameMode.h"
 #include "PTWCopsAndRobbersGameMode.generated.h"
 
-/**
- * 
- */
+#define ROBBERS 0
+#define COPS 1
+
+class UGameplayAbility;
+
 UCLASS()
 class PTW_API APTWCopsAndRobbersGameMode : public APTWMiniGameMode
 {
 	GENERATED_BODY()
 	
+public:
+	APTWCopsAndRobbersGameMode();
 	
+	virtual void StartGame() override;
+	virtual void WaitingToStartRound() override;
 	
-	
+protected:
+	UPROPERTY(EditAnywhere, Category="GAS")
+	TSubclassOf<UGameplayAbility> GA_Blind;
 };

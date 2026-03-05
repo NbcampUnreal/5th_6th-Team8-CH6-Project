17ea9b63a9e852e76fcaf98a1cf3b322bb3daccb
Jokimiddle <joki6180@gmail.com>
2026-03-05 15:20:01 +0900

    [add] 경찰과도둑 미니게임 기본 클래스 및 맵 추가

---
+++ PTW/Source/PTW/MiniGame/GameMode/PTWCopsAndRobbersGameMode.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MiniGame/PTWMiniGameMode.h"
+#include "PTWCopsAndRobbersGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PTW_API APTWCopsAndRobbersGameMode : public APTWMiniGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};

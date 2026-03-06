a4c066f01c6b298dc431d073bf488bbf41efc7d7
Jokimiddle <joki6180@gmail.com>
2026-03-06 14:16:30 +0900

    [update] 시민AI 구현 및 레벨에 배치 [update] NavigationSystem 활성화

--- PTW/Source/PTW/MiniGame/Character/AI/CopsAndRobbers/PTWCARCitizen.cpp
+++ PTW/Source/PTW/MiniGame/Character/AI/CopsAndRobbers/PTWCARCitizen.cpp
@@ -7,11 +7,11 @@
 // Sets default values
 APTWCARCitizen::APTWCARCitizen()
 {
-	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
+	
+	AutoPossessAI = EAutoPossessAI::PlacedInWorldOrSpawned;
 }
 
-// Called when the game starts or when spawned
 void APTWCARCitizen::BeginPlay()
 {
 	Super::BeginPlay();

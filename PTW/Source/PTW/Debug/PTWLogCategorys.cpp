dfeb43e29cdb6acf86770d48b08ac2ccbadb7b75
Wonjun - Seo <wonjun199908>
2026-03-09 21:11:25 +0900

    [add] 디버그용 로그 카테고리 추가 매크로 파일 생성

---
+++ PTW/Source/PTW/Debug/PTWLogCategorys.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Debug/PTWLogCategorys.h"
+
+
+
+// 사용방법 예시
+// UE_LOG(LogGameMode, Error, TEXT("Test"));
+
+PTW_API DEFINE_LOG_CATEGORY(LogGameMode);
---
+++ PTW/Source/PTW/Debug/PTWLogCategorys.h
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+
+
+//여기에 자신이 쓰는 로그 카테고리 만들어서 본인 작업 장소에서 헤더추가 해서 사용하기
+
+
+// 사용 예시
+PTW_API DECLARE_LOG_CATEGORY_EXTERN(LogGameMode, Log, All) // 게임모드 로그 

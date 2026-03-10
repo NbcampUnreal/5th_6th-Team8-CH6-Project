e9b3acc3198943b67b3fa150914904eb59b71c49
Wonjun - Seo <wonjun199908>
2026-03-10 10:29:32 +0900

    [fix] 로그 카테고리 수정

--- PTW/Source/PTW/Debug/PTWLogCategorys.h
+++ PTW/Source/PTW/Debug/PTWLogCategorys.h
@@ -4,10 +4,10 @@
 
 #include "CoreMinimal.h"
 
+/**
+ * 
+ */
 
+//여기에 로그 카테고리 만들어서 헤더추가해서 사용
 
-//여기에 자신이 쓰는 로그 카테고리 만들어서 본인 작업 장소에서 헤더추가 해서 사용하기
-
-
-// 사용 예시
-PTW_API DECLARE_LOG_CATEGORY_EXTERN(LogGameMode, Log, All) // 게임모드 로그 
+PTW_API DECLARE_LOG_CATEGORY_EXTERN(LogGameMode, Log, All);

4dc3442bf2741f10266cb2010b06613f44a54025
Wonjun - Seo <wonjun199908>
2026-03-11 12:02:43 +0900

    [update] 타이머 감소 함수에 로그 추가

--- PTW/Source/PTW/Debug/PTWLogCategorys.h
+++ PTW/Source/PTW/Debug/PTWLogCategorys.h
@@ -11,3 +11,7 @@
 //여기에 로그 카테고리 만들어서 헤더추가해서 사용
 
 PTW_API DECLARE_LOG_CATEGORY_EXTERN(Log_GameMode, Log, All);
+
+PTW_API DECLARE_LOG_CATEGORY_EXTERN(Log_GameState, Log, All);
+
+PTW_API DECLARE_LOG_CATEGORY_EXTERN(Log_AbilityBattle, Log, All);

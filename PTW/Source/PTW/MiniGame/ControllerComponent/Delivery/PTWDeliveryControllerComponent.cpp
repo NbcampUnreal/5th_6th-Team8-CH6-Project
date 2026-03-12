4b9385e3cd178839354a7adce3216a75aea796ed
jeongseoggi <93635959+jeongseoggi@users.noreply.github.com>
2026-03-12 12:43:02 +0900

    [Add] : ControllerComponent 디렉터리 및 클래스 추가

---
+++ PTW/Source/PTW/MiniGame/ControllerComponent/Delivery/PTWDeliveryControllerComponent.cpp
@@ -0,0 +1,23 @@
+﻿#include "PTWDeliveryControllerComponent.h"
+
+UPTWDeliveryControllerComponent::UPTWDeliveryControllerComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+
+
+}
+
+void UPTWDeliveryControllerComponent::AddBatteryUI()
+{
+	
+}
+
+
+// Called when the game starts
+void UPTWDeliveryControllerComponent::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+
+
---
+++ PTW/Source/PTW/MiniGame/ControllerComponent/Delivery/PTWDeliveryControllerComponent.h
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "PTWDeliveryControllerComponent.generated.h"
+
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class PTW_API UPTWDeliveryControllerComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	UPTWDeliveryControllerComponent();
+	
+	void AddBatteryUI();
+
+protected:
+	virtual void BeginPlay() override;
+
+
+
+};

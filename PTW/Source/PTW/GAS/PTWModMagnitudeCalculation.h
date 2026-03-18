c6b2f76e52f31559ea00bc68c3cd2562a58ad548
jeongseoggi <93635959+jeongseoggi@users.noreply.github.com>
2026-03-18 17:34:58 +0900

    [Add] : 데미지 계산용 MMC 클래스 추가

---
+++ PTW/Source/PTW/GAS/PTWModMagnitudeCalculation.h
@@ -0,0 +1,40 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayModMagnitudeCalculation.h"
+#include "PTWAbilityBattleAttributeSet.h"
+#include "PTWWeaponAttributeSet.h"
+#include "PTWModMagnitudeCalculation.generated.h"
+
+struct FPTWDamageStatics
+{
+	// 공통 무기 데미지
+	FGameplayEffectAttributeCaptureDefinition WeaponDamageDef;
+    
+	// 미니게임 A용 배율
+	FGameplayEffectAttributeCaptureDefinition GameADamageMulDef;
+	// 미니게임 B용 배율
+	FGameplayEffectAttributeCaptureDefinition GameBDamageMulDef;
+
+	FPTWDamageStatics()
+	{
+		// 1. 공통 무기 데미지 캐싱
+		WeaponDamageDef = FGameplayEffectAttributeCaptureDefinition(UPTWWeaponAttributeSet::GetDamageAttribute(), EGameplayEffectAttributeCaptureSource::Source, false);
+
+		// 2. 미니게임별 배율 캐싱 (각기 다른 클래스에서 가져옴)
+		//GameADamageMulDef = FGameplayEffectAttributeCaptureDefinition(UPTWAbilityBattleAttributeSet::Get(), EGameplayEffectAttributeCaptureSource::Source, false);
+	}
+	
+	
+};
+
+/**
+ * 
+ */
+UCLASS()
+class PTW_API UPTWModMagnitudeCalculation : public UGameplayModMagnitudeCalculation
+{
+	GENERATED_BODY()
+};

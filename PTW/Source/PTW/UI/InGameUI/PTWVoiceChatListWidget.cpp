41b6055269c040a57a8a5117e8987729f25ee0be
Jokimiddle <joki6180@gmail.com>
2026-03-18 19:12:49 +0900

    [add] VoiceChatWidget and VoiceChatListWidget 추가 및 구현

---
+++ PTW/Source/PTW/UI/InGameUI/PTWVoiceChatListWidget.cpp
@@ -0,0 +1,106 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PTWVoiceChatListWidget.h"
+#include "PTWVoiceChatWidget.h"
+#include "Components/VerticalBox.h"
+#include "GameFramework/GameStateBase.h"
+#include "GameFramework/PlayerState.h"
+#include "System/PTWVoiceChatSubsystem.h"
+
+void UPTWVoiceChatListWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+	
+	if (ULocalPlayer* LocalPlayer = GetOwningLocalPlayer())
+	{
+		if (UPTWVoiceChatSubsystem* VoiceChatSubsystem = LocalPlayer->GetSubsystem<UPTWVoiceChatSubsystem>())
+		{
+			VoiceChatSubsystem->OnVoiceStateUpdated.AddUniqueDynamic(this, &ThisClass::OnVoiceStateChanged);
+		}
+	}
+}
+
+void UPTWVoiceChatListWidget::NativeDestruct()
+{
+	if (ULocalPlayer* LocalPlayer = GetOwningLocalPlayer())
+	{
+		if (UPTWVoiceChatSubsystem* VoiceChatSubsystem = LocalPlayer->GetSubsystem<UPTWVoiceChatSubsystem>())
+		{
+			if (VoiceChatSubsystem->OnVoiceStateUpdated.IsAlreadyBound(this, &ThisClass::OnVoiceStateChanged))
+			{
+				VoiceChatSubsystem->OnVoiceStateUpdated.RemoveDynamic(this, &ThisClass::OnVoiceStateChanged);
+			}
+		}
+	}
+	
+	Super::NativeDestruct();
+}
+
+void UPTWVoiceChatListWidget::Init()
+{
+	
+}
+
+void UPTWVoiceChatListWidget::OnVoiceStateChanged(const FString& PlayerNetId, bool bIsTalking)
+{
+	if (PlayerNetId.IsEmpty() || !IsValid(VoiceChatListVerticalBox)) return;
+	
+	FString PlayerName = GetPlayerNameFromNetId(PlayerNetId);
+	UPTWVoiceChatWidget* TargetWidget = nullptr;
+    
+	for (UPTWVoiceChatWidget* Widget : VoiceChatWidgets)
+	{
+		if (IsValid(Widget) && PlayerName == Widget->GetTalkerName())
+		{
+			TargetWidget = Widget;
+			break;
+		}
+	}
+	
+	if (!IsValid(TargetWidget) && IsValid(VoiceChatWidgetClass))
+	{
+		TargetWidget = CreateWidget<UPTWVoiceChatWidget>(this, VoiceChatWidgetClass);
+		TargetWidget->SetupWidget(PlayerName);
+		VoiceChatWidgets.Add(TargetWidget);
+	}
+	
+	if (!IsValid(TargetWidget)) return;
+	
+	if (bIsTalking)
+	{
+		if (!VoiceChatListVerticalBox->HasChild(TargetWidget))
+		{
+			VoiceChatListVerticalBox->AddChildToVerticalBox(TargetWidget);
+		}
+	}
+	else
+	{
+		if (VoiceChatListVerticalBox->HasChild(TargetWidget))
+		{
+			VoiceChatListVerticalBox->RemoveChild(TargetWidget);
+		}
+	}
+}
+
+FString UPTWVoiceChatListWidget::GetPlayerNameFromNetId(const FString& TargetNetId)
+{
+	UWorld* World = GetWorld();
+	if (!World) return TEXT("Unknown");
+	
+	AGameStateBase* GameState = World->GetGameState();
+	if (!GameState) return TEXT("Unknown");
+	
+	for (APlayerState* PS : GameState->PlayerArray)
+	{
+		if (IsValid(PS))
+		{
+			const FUniqueNetIdRepl& TempNetId = PS->GetUniqueId();
+			if (TempNetId.IsValid() && TempNetId->ToString() == TargetNetId)
+			{
+				return PS->GetPlayerName();
+			}
+		}
+	}
+	return TEXT("Unknown");
+}
---
+++ PTW/Source/PTW/UI/InGameUI/PTWVoiceChatListWidget.h
@@ -0,0 +1,37 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "PTWVoiceChatListWidget.generated.h"
+
+class UVerticalBox;
+class UPTWVoiceChatWidget;
+/**
+ * 
+ */
+UCLASS()
+class PTW_API UPTWVoiceChatListWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+protected:
+	virtual void NativeConstruct() override;
+	virtual void NativeDestruct() override;
+public:
+	void Init();
+	UFUNCTION(BlueprintCallable, Category = "Voice Chat")
+	void OnVoiceStateChanged(const FString& PlayerNetId, bool bIsTalking);
+	
+	FString GetPlayerNameFromNetId(const FString& TargetNetId);
+protected:
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UVerticalBox> VoiceChatListVerticalBox;
+	
+	UPROPERTY(EditDefaultsOnly)
+	TSubclassOf<UPTWVoiceChatWidget> VoiceChatWidgetClass;
+	
+	UPROPERTY()
+	TArray<UPTWVoiceChatWidget*> VoiceChatWidgets;
+};

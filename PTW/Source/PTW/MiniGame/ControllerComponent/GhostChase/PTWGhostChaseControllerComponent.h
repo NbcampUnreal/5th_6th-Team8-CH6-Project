3000e4f237a99e458d01d7fd89909123e5cd1d62
KimSehun-712 <pitpgns314@naver.com>
2026-03-19 19:24:24 +0900

    [update] 꼬리잡기 맵 디자인 [fix] 타겟플레이어 사망 시 새로운 타겟의 시점으로 바뀌지 않는 현상 수정 [refactor] 타겟뷰 관련 로직 컨트롤러에서 컴포넌트로 이동

--- PTW/Source/PTW/MiniGame/ControllerComponent/GhostChase/PTWGhostChaseControllerComponent.h
+++ PTW/Source/PTW/MiniGame/ControllerComponent/GhostChase/PTWGhostChaseControllerComponent.h
@@ -8,6 +8,8 @@
 
 class UWidgetComponent;
 class UUserWidget;
+class UTextureRenderTarget2D;
+class USceneCaptureComponent2D;
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class PTW_API UPTWGhostChaseControllerComponent : public UActorComponent
@@ -24,13 +26,36 @@ public:
 
 	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
 
+	UFUNCTION()
+	void OnRep_CurrentTargetPawn();
+
+	/* POV 관련 함수 */
+	void UpdateTargetPOV(APawn* NewTarget);
+	void RefreshTargetViewHiddenActors();
+	void CaptureTargetPOV();
+	UFUNCTION(Client, Reliable)
+	void Client_UpdateTargetPOV(APawn* NewTarget);
+
 	/* 이름 강조 */
 	void ApplyNameTagHighlight(APawn* TargetPawn, UWidgetComponent* WidgetComp);
 
 protected:
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+
 	UPROPERTY(Replicated)
 	TObjectPtr<APawn> CurrentTargetPawn;
-		
+	
+	UPROPERTY()
+	TObjectPtr<UTextureRenderTarget2D> TargetPOVRT;
+
+	UPROPERTY()
+	TObjectPtr<USceneCaptureComponent2D> CurrentActiveCapture;
+
+	FTimerHandle POVCaptureTimerHandle;
+
+	UPROPERTY(EditAnywhere, Category = "GhostChase")
+	TSubclassOf<UUserWidget> POVWidgetClass;
+
 	UPROPERTY(EditAnywhere, Category = "GhostChase")
 	FLinearColor TargetHighlightColor = FLinearColor::Red; // 강조 색상
 };

e764106ecc59fe1496de8e0b2198bcc62140bc70
Jokimiddle <joki6180@gmail.com>
2026-03-19 19:27:28 +0900

    [add] CARControllerComponent 추가 및 구현 [update] CAR 미니게임 로직 추가

---
+++ PTW/Source/PTW/MiniGame/ControllerComponent/CopsAndRobbers/PTWCARControllerComponent.h
@@ -0,0 +1,34 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "PTWCARControllerComponent.generated.h"
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class PTW_API UPTWCARControllerComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UPTWCARControllerComponent();
+
+protected:
+	virtual void BeginPlay() override;
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+	
+public:
+	virtual void InitializeController();
+	void DestroyOtherTeamNameTag();
+	void SetTeamId(int32 NewTeamId);	// ServerOnly Invoke
+	
+	UFUNCTION()
+	void OnRep_TeamId();
+	
+protected:
+	UPROPERTY(ReplicatedUsing=OnRep_TeamId)
+	int32 TeamId = -1;
+};

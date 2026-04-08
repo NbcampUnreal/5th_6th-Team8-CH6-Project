9b2098cdfa4e7ecf16f6b47237fbfd0e2e4fd3e3
Jokimiddle <joki6180@gmail.com>
2026-04-08 16:00:51 +0900

    [Refactor] Steam, GameLift 서브시스템들 리팩토링 및 불필요 주석 제거

--- PTW/Source/PTW/System/PTWGameLiftClientSubsystem.cpp
+++ PTW/Source/PTW/System/PTWGameLiftClientSubsystem.cpp
@@ -1,7 +1,4 @@
-﻿// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "PTWGameLiftClientSubsystem.h"
+﻿#include "PTWGameLiftClientSubsystem.h"
 #include "Server/GameplayServerTags.h"
 #include "Server/PTWAPIData.h"
 #include "HttpModule.h"
@@ -12,9 +9,8 @@
 #include "Online/OnlineSessionNames.h"
 #include "Session/PTWSessionConfig.h"
 #include "UObject/Object.h"
-
-DEFINE_LOG_CATEGORY(GameLift);
-DEFINE_LOG_CATEGORY(DynamoDB);
+#include "Utilities/PTWJsonUtility.h"
+#include "Debug/PTWLogCategorys.h"
 
 #define LOCTEXT_NAMESPACE "GAMELIFTCLIENTSUBSYSTEM"
 
@@ -53,63 +49,20 @@ void UPTWGameLiftClientSubsystem::Deinitialize()
 	Super::Deinitialize();
 }
 
-FString UPTWGameLiftClientSubsystem::SerializeJsonContent(const TMap<FString, FString>& Parameters)
+FString UPTWGameLiftClientSubsystem::GetUniquePlayerId() const
 {
-	TSharedPtr<FJsonObject> ContentJsonObject = MakeShareable(new FJsonObject());
-	
-	// Lambda 함수와 동일한 필드로 설정.
-	// ContentJsonObject->SetStringField(TEXT("playerId"), PlayerId);
-	// ContentJsonObject->SetStringField(TEXT("gameSessionId"), GameSessionId);
-	for (const auto& Param : Parameters)
+	if (APlayerController* LocalPlayerController = GEngine->GetFirstLocalPlayerController(GetWorld()))
 	{
-		ContentJsonObject->SetStringField(Param.Key, Param.Value);
-	}
-	
-	FString Content;
-	TSharedRef<TJsonWriter<>> JsonWriter = TJsonWriterFactory<>::Create(&Content);
-	FJsonSerializer::Serialize(ContentJsonObject.ToSharedRef(), JsonWriter);
-	
-	return Content;
-}
-
-TMap<FString, FString> UPTWGameLiftClientSubsystem::ExtractJsonFields(const FString& JsonString, const TArray<FString>& TargetFields)
-{
-	TMap<FString, FString> ResultMap;
-
-	TSharedPtr<FJsonObject> JsonObject;
-	TSharedRef<TJsonReader<>> Reader = TJsonReaderFactory<>::Create(JsonString);
-    
-	if (FJsonSerializer::Deserialize(Reader, JsonObject) && JsonObject.IsValid())
-	{
-		const TSharedPtr<FJsonObject>* DataObjPtr = nullptr;
-
-		if (JsonObject->TryGetObjectField(TEXT("data"), DataObjPtr) && DataObjPtr != nullptr && (*DataObjPtr).IsValid())
+		if (APlayerState* LocalPlayerState = LocalPlayerController->GetPlayerState<APlayerState>())
 		{
-			for (const FString& FieldName : TargetFields)
+			if (LocalPlayerState->GetUniqueId().IsValid())
 			{
-				TSharedPtr<FJsonValue> JsonValue = (*DataObjPtr)->TryGetField(FieldName);
-          
-				if (JsonValue.IsValid() && !JsonValue->IsNull())
-				{
-					ResultMap.Add(FieldName, JsonValue->AsString());
-				}
-				else
-				{
-					ResultMap.Add(FieldName, TEXT("")); 
-				}
+				const FString UniqueId = TEXT("Player_") + FString::FromInt(LocalPlayerState->GetUniqueID());
+				return UniqueId;
 			}
 		}
-		else
-		{
-			UE_LOG(LogTemp, Warning, TEXT("Helper: 'data' 객체를 찾을 수 없거나 유효하지 않습니다."));
-		}
 	}
-	else
-	{
-		UE_LOG(LogTemp, Warning, TEXT("Helper: JSON 파싱에 실패했습니다."));
-	}
-
-	return ResultMap;
+	return FString();
 }
 
 void UPTWGameLiftClientSubsystem::CreateGameSession(FPTWSessionConfig& SessionConfig)
@@ -135,11 +88,11 @@ void UPTWGameLiftClientSubsystem::CreateGameSession(FPTWSessionConfig& SessionCo
 	};
 	
 	GetWorld()->GetTimerManager().SetTimer(CheckSessionLitmitTimer, 20.0f, false);
-	const FString Content = SerializeJsonContent(Params);
+	const FString Content = UPTWJsonUtility::MakeHTTPRequestBody(Params);
 	Request->SetContentAsString(Content);
 	if (Request->ProcessRequest())
 	{
-		UE_LOG(GameLift, Display, TEXT("[게임세션 생성요청] 게임리프트 게임세션 생성요청 전송 완료"));
+		UE_LOG(Log_GameLift, Display, TEXT("[게임세션 생성요청] 게임리프트 게임세션 생성요청 전송 완료"));
 	}
 	else
 	{
@@ -148,7 +101,7 @@ void UPTWGameLiftClientSubsystem::CreateGameSession(FPTWSessionConfig& SessionCo
 			FText ErrorMessage = LOCTEXT("HTTPRequestFailed", "네트워크 문제로 인해 서버에 세션 생성 요청을 보내지 못했습니다. 연결 상태를 확인해 주세요.");
 			OnGameLiftSessionMessageReceived.Broadcast(ErrorMessage);
 		}
-		UE_LOG(GameLift, Error, TEXT("[게임세션 생성요청] 게임리프트 게임세션 생성요청 전송 실패"));
+		UE_LOG(Log_GameLift, Error, TEXT("[게임세션 생성요청] 게임리프트 게임세션 생성요청 전송 실패"));
 		GetWorld()->GetTimerManager().ClearTimer(CheckSessionLitmitTimer);
 	}
 }
@@ -162,13 +115,13 @@ void UPTWGameLiftClientSubsystem::CreateGameSession_Response(FHttpRequestPtr Req
 			FText ErrorMessage = LOCTEXT("SessionCreateFailed", "알 수 없는 오류가 발생해 세션 생성에 실패했습니다.");
 			OnGameLiftSessionMessageReceived.Broadcast(ErrorMessage);
 		}
-		UE_LOG(GameLift, Error, TEXT("[게임세션 생성응답] 게임리프트 게임세션 생성 실패 응답 (Code:%d)"), Response->GetResponseCode());
+		UE_LOG(Log_GameLift, Error, TEXT("[게임세션 생성응답] 게임리프트 게임세션 생성 실패 응답 (Code:%d)"), Response->GetResponseCode());
 		return;
 	}
 	FString GameSessionIdName = TEXT("GameSessionId");
 	TArray<FString> FieldsToExtract = { GameSessionIdName };
 	
-	TMap<FString, FString> SessionData = ExtractJsonFields(Response->GetContentAsString(), FieldsToExtract);
+	TMap<FString, FString> SessionData = UPTWJsonUtility::ExtractFieldsFromHTTPData(Response->GetContentAsString(), FieldsToExtract);
 	
 	if (SessionData.Contains(GameSessionIdName))
 	{
@@ -202,7 +155,7 @@ void UPTWGameLiftClientSubsystem::CheckSessionStatus(const FString& SessionId, b
 	Request->SetHeader(TEXT("Content-Type"), TEXT("application/json"));
 	if (Request->ProcessRequest())
 	{
-		UE_LOG(DynamoDB, Display, TEXT("[DB속성 확인요청] DB:GameSessionLists ServerState 확인요청 전송성공 (GameSessionId:%s"), *SessionId);
+		UE_LOG(Log_DynamoDB, Display, TEXT("[DB속성 확인요청] DB:GameSessionLists ServerState 확인요청 전송성공 (GameSessionId:%s"), *SessionId);
 	}
 	else
 	{
@@ -211,7 +164,7 @@ void UPTWGameLiftClientSubsystem::CheckSessionStatus(const FString& SessionId, b
 			FText ErrorMessage = LOCTEXT("HTTPRequestFailed", "네트워크 문제로 인해 서버에 세션 생성 요청을 보내지 못했습니다. 연결 상태를 확인해 주세요.");
 			OnGameLiftSessionMessageReceived.Broadcast(ErrorMessage);
 		}
-		UE_LOG(DynamoDB, Error, TEXT("[DB속성 확인요청] DB:GameSessionLists ServerState 확인요청 전송실패 (GameSessionId:%s"), *SessionId);
+		UE_LOG(Log_DynamoDB, Error, TEXT("[DB속성 확인요청] DB:GameSessionLists ServerState 확인요청 전송실패 (GameSessionId:%s"), *SessionId);
 		GetWorld()->GetTimerManager().ClearTimer(CheckSessionLitmitTimer);
 	}
 }
@@ -225,7 +178,7 @@ void UPTWGameLiftClientSubsystem::CheckSessionStatus_Response(FHttpRequestPtr Re
 			FText ErrorMessage = LOCTEXT("SessionCheckFailed", "알 수 없는 오류가 발생해 세션 체크에 실패하였습니다.");
 			OnGameLiftSessionMessageReceived.Broadcast(ErrorMessage);
 		}
-		UE_LOG(DynamoDB, Error, TEXT("[DB속성 확인응답] DB:GameSessionLists ServerState 확인 실패"));
+		UE_LOG(Log_DynamoDB, Error, TEXT("[DB속성 확인응답] DB:GameSessionLists ServerState 확인 실패"));
 	}
 }
 
@@ -240,20 +193,20 @@ void UPTWGameLiftClientSubsystem::CheckSessionStatusLoop_Response(FHttpRequestPt
 			FText ErrorMessage = LOCTEXT("SessionCheckFailed", "알 수 없는 오류가 발생해 세션 체크에 실패하였습니다.");
 			OnGameLiftSessionMessageReceived.Broadcast(ErrorMessage);
 		}
-		UE_LOG(DynamoDB, Error, TEXT("[DB속성 확인응답] DB:GameSessionLists ServerState 확인실패"));
+		UE_LOG(Log_DynamoDB, Error, TEXT("[DB속성 확인응답] DB:GameSessionLists ServerState 확인실패"));
 	}
 	else if (bWasSuccessful && Response.IsValid())
 	{
-		UE_LOG(DynamoDB, Error, TEXT("[DB속성 확인응답] DB:GameSessionLists ServerState 확인성공"));
+		UE_LOG(Log_DynamoDB, Error, TEXT("[DB속성 확인응답] DB:GameSessionLists ServerState 확인성공"));
 		if (Response->GetResponseCode() == 202)
 		{
-			UE_LOG(DynamoDB, Display, TEXT("[DB속성 확인응답] DB:GameSessionLists ServerState: ACTIVATING (GameSessionId:%s"), *SessionId);
+			UE_LOG(Log_DynamoDB, Display, TEXT("[DB속성 확인응답] DB:GameSessionLists ServerState: ACTIVATING (GameSessionId:%s"), *SessionId);
 			WaitForSessionActivation(SessionId);
 		}
 		else if (Response->GetResponseCode() == 200)
 		{
 			GetWorld()->GetTimerManager().ClearTimer(CheckSessionLitmitTimer);
-			UE_LOG(DynamoDB, Display, TEXT("[DB속성 확인응답] DB:GameSessionLists ServerState: ACTIVE (GameSessionId:%s"), *SessionId);
+			UE_LOG(Log_DynamoDB, Display, TEXT("[DB속성 확인응답] DB:GameSessionLists ServerState: ACTIVE (GameSessionId:%s"), *SessionId);
 			CreatePlayerSession(GetUniquePlayerId(), SessionId);
 		}
 	}
@@ -269,7 +222,7 @@ void UPTWGameLiftClientSubsystem::WaitForSessionActivation(const FString& Sessio
 			PC->GetWorldTimerManager().SetTimer(CreateSessionTimer,[this, SessionId]()
 			{
 				CheckSessionStatus(SessionId, true);
-				UE_LOG(LogTemp, Display, TEXT("CheckSessionStatus 재시도"));
+				UE_LOG(Log_GameLift, Display, TEXT("CheckSessionStatus 재시도"));
 			}, 2.0f, false);
 		}
 		else
@@ -278,48 +231,8 @@ void UPTWGameLiftClientSubsystem::WaitForSessionActivation(const FString& Sessio
 			{
 				FText ErrorMessage = LOCTEXT("SessionCheckTimeOut", "세션 타임아웃이 발생했습니다.");
 				OnGameLiftSessionMessageReceived.Broadcast(ErrorMessage);
-				UE_LOG(LogTemp, Warning, TEXT("WaitForSessionActivation 타임아웃"));
 			}
 		}
-	}
-}
-
-void UPTWGameLiftClientSubsystem::DescribeGameSession(const FString& SessionId)
-{
-	if (!IsValid(ClientAPIData))
-	{
-		UE_LOG(LogTemp, Error, TEXT("DescribeGameSession Failed: APIData is NULL!"));
-		return;
-	}
-
-	TSharedRef<IHttpRequest> Request = FHttpModule::Get().CreateRequest();
-	Request->OnProcessRequestComplete().BindUObject(this, &ThisClass::DescribeGameSession_Response);
-	
-	const FString APIUrl = ClientAPIData->GetAPIEndPoint(GameplayServerTags::GameSessionsAPI::DescribeGameSession);
-	FString RefinedURL = APIUrl;
-	
-	RefinedURL += TEXT("?gameSessionId=") + FGenericPlatformHttp::UrlEncode(SessionId);
-	
-	Request->SetURL(RefinedURL);
-	Request->SetVerb(TEXT("GET"));
-	Request->SetHeader(TEXT("Content-Type"), TEXT("application/json"));
-	Request->ProcessRequest();
-}
-
-void UPTWGameLiftClientSubsystem::DescribeGameSession_Response(FHttpRequestPtr Request, FHttpResponsePtr Response, bool bWasSuccessful)
-{
-	if (!bWasSuccessful || !Response.IsValid() || Response->GetResponseCode() != 200)
-	{
-		UE_LOG(LogTemp, Error, TEXT("DescribeGameSession 통신 실패."));
-		return;
-	}
-	
-	FPTWGameLiftGameSession GameSession;
-	if (ParseDataFromJson<FPTWGameLiftGameSession>(Response->GetContentAsString(), GameSession))
-	{
-		const FString GameSessionId = GameSession.GameSessionId;
-		const FString GameSessionStatus = GameSession.Status;
-		// TryJoinGameSession(GameSessionStatus, GameSessionId);
 	}
 }
 
@@ -338,12 +251,12 @@ void UPTWGameLiftClientSubsystem::CreatePlayerSession(const FString& PlayerId, c
 		{ TEXT("gameSessionId"), GameSessionId }
 	};
 	
-	const FString Content = SerializeJsonContent(Params);
+	const FString Content = UPTWJsonUtility::MakeHTTPRequestBody(Params);
 	
 	Request->SetContentAsString(Content);
 	if (Request->ProcessRequest())
 	{
-		UE_LOG(GameLift, Display, TEXT("[플레이어세션 생성요청] 게임리프트 게임세션 - 플레이어세션 생성요청 전송완료 (GameSessionId:%s)"), *GameSessionId);
+		UE_LOG(Log_GameLift, Display, TEXT("[플레이어세션 생성요청] 게임리프트 게임세션 - 플레이어세션 생성요청 전송완료 (GameSessionId:%s)"), *GameSessionId);
 	}
 	else
 	{
@@ -352,15 +265,13 @@ void UPTWGameLiftClientSubsystem::CreatePlayerSession(const FString& PlayerId, c
 			FText ErrorMessage = LOCTEXT("HTTPRequestFailed", "네트워크 문제로 인해 서버에 세션 생성 요청을 보내지 못했습니다. 연결 상태를 확인해 주세요.");
 			OnGameLiftSessionMessageReceived.Broadcast(ErrorMessage);
 		}
-		UE_LOG(GameLift, Error, TEXT("[플레이어세션 생성요청] 게임리프트 게임세션 - 플레이어세션 생성요청 전송실패 (GameSessionId:%s)"), *GameSessionId);
+		UE_LOG(Log_GameLift, Error, TEXT("[플레이어세션 생성요청] 게임리프트 게임세션 - 플레이어세션 생성요청 전송실패 (GameSessionId:%s)"), *GameSessionId);
 		GetWorld()->GetTimerManager().ClearTimer(CheckSessionLitmitTimer);
 	}
 }
 
 void UPTWGameLiftClientSubsystem::CreatePlayerSession_Response(FHttpRequestPtr Request, FHttpResponsePtr Response, bool bWasSuccessful)
 {
-	UE_LOG(LogTemp, Warning, TEXT("Create PlayerSession Response Received"));
-	
 	if (!bWasSuccessful || !Response.IsValid() || Response->GetResponseCode() != 200)
 	{
 		if (OnGameLiftSessionMessageReceived.IsBound())
@@ -368,21 +279,20 @@ void UPTWGameLiftClientSubsystem::CreatePlayerSession_Response(FHttpRequestPtr R
 			FText ErrorMessage = LOCTEXT("SessionCheckTimeOut", "세션 참여에 실패하였습니다.");
 			OnGameLiftSessionMessageReceived.Broadcast(ErrorMessage);
 		}
-		UE_LOG(GameLift, Error, TEXT("[플레이어세션 생성응답] 게임리프트 게임세션 - 플레이어세션 생성실패 응답 (Code:%d)"), Response->GetResponseCode());
+		UE_LOG(Log_GameLift, Error, TEXT("[플레이어세션 생성응답] 게임리프트 게임세션 - 플레이어세션 생성실패 응답 (Code:%d)"), Response->GetResponseCode());
 		return;
 	}
-	UE_LOG(GameLift, Display, TEXT("[플레이어세션 생성응답] 게임리프트 게임세션 - 플레이어세션 생성성공 응답 (Code:%d)"), Response->GetResponseCode());
+	UE_LOG(Log_GameLift, Display, TEXT("[플레이어세션 생성응답] 게임리프트 게임세션 - 플레이어세션 생성성공 응답 (Code:%d)"), Response->GetResponseCode());
 	
 	FString PlayerSessionIdName = TEXT("PlayerSessionId");
 	FString PortName = TEXT("Port");
 	FString SteamIdName = TEXT("SteamId");
 	
 	TArray<FString> FieldsToExtract = { PlayerSessionIdName, PortName, SteamIdName };
-	TMap<FString, FString> SessionData = ExtractJsonFields(Response->GetContentAsString(), FieldsToExtract);
+	TMap<FString, FString> SessionData = UPTWJsonUtility::ExtractFieldsFromHTTPData(Response->GetContentAsString(), FieldsToExtract);
 	if (SessionData.Contains(PlayerSessionIdName) && SessionData.Contains(PortName) && SessionData.Contains(SteamIdName))
 	{
 		// ("steam.%s:%s?PlayerSessionId=%s")
-		
 		FString ConnectURL = FString::Printf(TEXT("?PlayerSessionId=%s"), *SessionData[PlayerSessionIdName]);
 		UE_LOG(LogTemp, Warning, TEXT("%s"), *ConnectURL);
 		
@@ -414,7 +324,7 @@ void UPTWGameLiftClientSubsystem::SearchGameSessions_Response(FHttpRequestPtr Re
 	}
 	
 	TArray<FPTWGameSessionListsTable> GameSessionLists;
-	ParseDataArrayFromJson(Response->GetContentAsString(), GameSessionLists);
+	UPTWJsonUtility::ParseHTTPDataToArray(Response->GetContentAsString(), GameSessionLists);
 	if (GameSessionLists.IsEmpty()) return;
 	
 	if (OnSessionSearchComplete.IsBound())
@@ -437,7 +347,7 @@ void UPTWGameLiftClientSubsystem::SearchQuickSession()
 	GetWorld()->GetTimerManager().SetTimer(CheckSessionLitmitTimer, 20.f, false);
 	if (Request->ProcessRequest())
 	{
-		UE_LOG(DynamoDB, Display, TEXT("[DB레코드 추출요청] DB:GameSessionLists 레코드 추출요청 전송성공"));
+		UE_LOG(Log_DynamoDB, Display, TEXT("[DB레코드 추출요청] DB:GameSessionLists 레코드 추출요청 전송성공"));
 	}
 	else
 	{
@@ -446,7 +356,7 @@ void UPTWGameLiftClientSubsystem::SearchQuickSession()
 			FText ErrorMessage = LOCTEXT("HTTPRequestFailed", "네트워크 문제로 인해 서버에 세션 생성 요청을 보내지 못했습니다. 연결 상태를 확인해 주세요.");
 			OnGameLiftSessionMessageReceived.Broadcast(ErrorMessage);
 		}
-		UE_LOG(DynamoDB, Error, TEXT("[DB레코드 추출요청] DB:GameSessionLists 레코드 추출요청 전송실패"));
+		UE_LOG(Log_DynamoDB, Error, TEXT("[DB레코드 추출요청] DB:GameSessionLists 레코드 추출요청 전송실패"));
 		GetWorld()->GetTimerManager().ClearTimer(CheckSessionLitmitTimer);
 	}
 }
@@ -461,13 +371,13 @@ void UPTWGameLiftClientSubsystem::SearchQuickSession_Response(FHttpRequestPtr Re
 			FText ErrorMessage = LOCTEXT("SessionCheckTimeOut", "세션 참여에 실패하였습니다.");
 			OnGameLiftSessionMessageReceived.Broadcast(ErrorMessage);
 		}
-		UE_LOG(DynamoDB, Error, TEXT("[DB레코드 추출응답] DB:GameSessionLists 레코드 추출실패 응답 (Code:%d)"), Response->GetResponseCode());
+		UE_LOG(Log_DynamoDB, Error, TEXT("[DB레코드 추출응답] DB:GameSessionLists 레코드 추출실패 응답 (Code:%d)"), Response->GetResponseCode());
 		return;
 	}
-	UE_LOG(DynamoDB, Display, TEXT("[DB레코드 추출응답] DB:GameSessionLists 레코드 추출성공 응답 (Code:%d)"), Response->GetResponseCode());
+	UE_LOG(Log_DynamoDB, Display, TEXT("[DB레코드 추출응답] DB:GameSessionLists 레코드 추출성공 응답 (Code:%d)"), Response->GetResponseCode());
 	
 	TArray<FPTWGameSessionListsTable> GameSessionList;
-	ParseDataArrayFromJson(Response->GetContentAsString(), GameSessionList);
+	UPTWJsonUtility::ParseHTTPDataToArray(Response->GetContentAsString(), GameSessionList);
 	
 	TArray<FPTWGameSessionListsTable> ActiveList;
 	TArray<FPTWGameSessionListsTable> ActivatingList;
@@ -484,8 +394,8 @@ void UPTWGameLiftClientSubsystem::SearchQuickSession_Response(FHttpRequestPtr Re
 		}
 	}
 	
-	UE_LOG(DynamoDB, Display, TEXT("[DB레코드 추출응답] DB:GameSessionLists ServerState:ACTIVE 서버:%d"), ActiveList.Num());
-	UE_LOG(DynamoDB, Display, TEXT("[DB레코드 추출응답] DB:GameSessionLists ServerState:ACTIVATING 서버:%d"), ActivatingList.Num());
+	UE_LOG(Log_DynamoDB, Display, TEXT("[DB레코드 추출응답] DB:GameSessionLists ServerState:ACTIVE 서버:%d"), ActiveList.Num());
+	UE_LOG(Log_DynamoDB, Display, TEXT("[DB레코드 추출응답] DB:GameSessionLists ServerState:ACTIVATING 서버:%d"), ActivatingList.Num());
 	
 	if (!ActiveList.IsEmpty())
 	{
@@ -499,7 +409,7 @@ void UPTWGameLiftClientSubsystem::SearchQuickSession_Response(FHttpRequestPtr Re
 	}
 	else
 	{
-		UE_LOG(LogTemp, Display, TEXT("유효한 서버 비존재. 직접서버 생성"));
+		UE_LOG(LogTemp, Display, TEXT("[DB레코드 추출응답] 유효한 서버 부재. 직접서버 생성"));
 		FPTWSessionConfig SessionConfig;
 		SessionConfig.ServerName = "QuickMatchGameSessions";
 		SessionConfig.MaxPlayers = 8;
@@ -574,22 +484,6 @@ void UPTWGameLiftClientSubsystem::FindByIdAndJoinSession(const FString& SteamId,
 			OnGameLiftSessionMessageReceived.Broadcast(ErrorMessage);
 		}
 	}
-}
-
-FString UPTWGameLiftClientSubsystem::GetUniquePlayerId() const
-{
-	if (APlayerController* LocalPlayerController = GEngine->GetFirstLocalPlayerController(GetWorld()))
-	{
-		if (APlayerState* LocalPlayerState = LocalPlayerController->GetPlayerState<APlayerState>())
-		{
-			if (LocalPlayerState->GetUniqueId().IsValid())
-			{
-				const FString UniqueId = TEXT("Player_") + FString::FromInt(LocalPlayerState->GetUniqueID());
-				return UniqueId;
-			}
-		}
-	}
-	return FString();
 }
 
 #undef LOCTEXT_NAMESPACE

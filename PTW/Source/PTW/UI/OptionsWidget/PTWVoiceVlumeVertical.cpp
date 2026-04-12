98479aa770852c96a5223fd1150a31b10fa5a946
Jokimiddle <joki6180@gmail.com>
2026-04-12 18:46:55 +0900

    [fix] VoiceVolumeVeritcal 오류 수정

--- PTW/Source/PTW/UI/OptionsWidget/PTWVoiceVlumeVertical.cpp
+++ PTW/Source/PTW/UI/OptionsWidget/PTWVoiceVlumeVertical.cpp
@@ -14,42 +14,50 @@ void UPTWVoiceVlumeVertical::InitWidget()
 		TMap<FString, FPTWPlayerVoiceInfo>& PlayerVoiceInfoList = VoiceChatSubsystem->PlayerVoiceInfoList;
 		if (!IsValid(VoiceVolumeClass)) return;
 		
-		// for (auto PlayerVoiceInfo : PlayerVoiceInfoList)
-		// {
-		// 	if (!PlayerVoiceVolumes.Contains(PlayerVoiceInfo.Key))
-		// 	{
-		// 		APlayerState* TargetPlayerState = nullptr;
-		// 		for (APlayerState* PS : GetWorld()->GetGameState()->PlayerArray)
-		// 		{
-		// 			if (PlayerVoiceInfo.Key == PS->GetUniqueId().ToString())
-		// 			{
-		// 				TargetPlayerState = PS;
-		// 				break;
-		// 			}
-		// 		}
-		//		if (!TargetPlayerState) return;
-		
 		for (APlayerState* PS : GetWorld()->GetGameState()->PlayerArray)
 		{
-			UPTWVoiceVolume* PlayerVoiceVolume = CreateWidget<UPTWVoiceVolume>(this, VoiceVolumeClass);
-			PlayerVoiceVolume->InitWidget(PS);
-		
-			USizeBox* SizeBox = NewObject<USizeBox>(this);
-			SizeBox->AddChild(PlayerVoiceVolume);
-			SizeBox->SetMinDesiredHeight(60.f);
-		
-			UVerticalBoxSlot* SizeBoxSlot = AddChildToVerticalBox(SizeBox);
-			SizeBoxSlot->SetHorizontalAlignment(HAlign_Fill);
-			SizeBoxSlot->SetVerticalAlignment(VAlign_Fill);
-			
-			if (PS == GetOwningPlayer()->GetPlayerState<APlayerState>())
-			{
-				// SizeBox->SetVisibility(ESlateVisibility::Collapsed);
-			}
 			FString UniqueId = PS->GetUniqueId().ToString();
-			if (!PlayerVoiceVolumes.Contains(UniqueId))
+			if (!PlayerVoiceInfoList.Contains(UniqueId))
 			{
-				PlayerVoiceVolumes.Add(UniqueId, PlayerVoiceVolume);
+				PlayerVoiceInfoList.Add(UniqueId, FPTWPlayerVoiceInfo());
+			}
+		}
+		for (auto PlayerVoiceInfoEntry : PlayerVoiceInfoList)
+		{
+			if (!PlayerVoiceVolumes.Contains(PlayerVoiceInfoEntry.Key))
+			{
+				APlayerState* TargetPS = nullptr;
+				for (APlayerState* PS : GetWorld()->GetGameState()->PlayerArray)
+				{
+					FString UniqueId = PS->GetUniqueId().ToString();
+					if (PlayerVoiceInfoEntry.Key == UniqueId)
+					{
+						TargetPS = PS;
+						break;
+					}
+				}
+				if (!IsValid(TargetPS)) continue;
+				
+				UPTWVoiceVolume* PlayerVoiceVolume = CreateWidget<UPTWVoiceVolume>(this, VoiceVolumeClass);
+				PlayerVoiceVolume->InitWidget(TargetPS);
+		
+				USizeBox* SizeBox = NewObject<USizeBox>(this);
+				SizeBox->AddChild(PlayerVoiceVolume);
+				SizeBox->SetMinDesiredHeight(60.f);
+		
+				UVerticalBoxSlot* SizeBoxSlot = AddChildToVerticalBox(SizeBox);
+				SizeBoxSlot->SetHorizontalAlignment(HAlign_Fill);
+				SizeBoxSlot->SetVerticalAlignment(VAlign_Fill);
+			
+				if (TargetPS == GetOwningPlayer()->GetPlayerState<APlayerState>())
+				{
+					SizeBox->SetVisibility(ESlateVisibility::Collapsed);
+				}
+				FString UniqueId = TargetPS->GetUniqueId().ToString();
+				if (!PlayerVoiceVolumes.Contains(UniqueId))
+				{
+					PlayerVoiceVolumes.Add(UniqueId, PlayerVoiceVolume);
+				}
 			}
 		}
 	}
@@ -63,10 +71,5 @@ TSharedRef<SWidget> UPTWVoiceVlumeVertical::RebuildWidget()
 
 void UPTWVoiceVlumeVertical::ReleaseSlateResources(bool bReleaseChildren)
 {
-	for (int32 i = 1; i < GetChildrenCount(); i++)
-	{
-		GetChildAt(1)->RemoveFromParent();
-	}
-	PlayerVoiceVolumes.Empty();
 	Super::ReleaseSlateResources(bReleaseChildren);
 }
